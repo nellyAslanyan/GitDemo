e794204fea7fe9c577bfb37f5d653716580d7cf6
nellyAslanyan <nellyaslanyan1997@gmail.com>
2024-12-14 13:51:19 +0400

    Initial Commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Nelly Aslanyan on 12/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
